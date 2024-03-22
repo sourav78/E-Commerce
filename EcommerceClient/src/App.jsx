c7480a5caf3193bd4f572e66f29868ca30dfc27d
sourav78 <souravranjan488@gmail.com>
2024-03-22 17:46:56 +0530

    frontend - add navigation in page

--- EcommerceClient/src/App.jsx
+++ EcommerceClient/src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
+import NavBar from "./components/NavBar";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+
+import poster1 from "./assets/poster1.jpg";
+import poster2 from "./assets/poster2.jpg";
+import poster3 from "./assets/poster3.jpg";
 
 const App = () => {
   return (
     <>
-      <div className="text-orange-600">Hello Ecommerce</div>
+      <div className=" min-h-screen border border-black">
+        <NavBar />
+        
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ EcommerceClient/src/components/NavBar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import s78_black from "../assets/S78_b.png";
+import { FiShoppingCart } from "react-icons/fi";
+import { FaBars } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
+
+const NavBar = () => {
+
+    const [openNav, setOpenNav] = useState(false)
+
+
+
+
+    return (
+        <>
+            <div className="py-3 sm:px-16 p-4 border-black bg-gray-100 shadow-md flex justify-between items-center z-10">
+                <div className="sm:w-32 w-24 flex items-end">
+                    <img src={s78_black} alt="" />
+                    <p className="text-4xl font-semibold">Store</p>
+                </div>
+                <div className="nav-items flex items-center overflow-x-hidden">
+                    <ul className={`sm:flex z-20 items-center gap-11 text-2xl font-semibold sm:flex-row flex-col sm:static absolute top-0 ${openNav ? `right-0` : `right-full`}  sm:w-auto w-3/4 sm:bg-transparent bg-slate-200 sm:h-auto h-screen p-1`}>
+                        <li className=" mt-6 sm:hidden p-1 inline-block bg-black text-white"
+                            onClick={() => setOpenNav(false)}
+                        >
+                            <RxCross2 />
+                        </li>
+                        <li 
+                            onClick={() => setOpenNav(false)}
+                        className=" hover:text-white hover:bg-black transition-all cursor-pointer mt-4 text-white sm:text-black border sm:border-none border-gray-300 sm:p-0 sm:px-2 p-2 rounded-md sm:bg-transparent bg-slate-500">
+                            Home
+                        </li>
+                        <li 
+                            onClick={() => setOpenNav(false)}
+                        className=" hover:text-white hover:bg-black transition-all cursor-pointer mt-4 text-white sm:text-black border sm:border-none border-gray-300 sm:p-0 sm:px-2 p-2 rounded-md sm:bg-transparent bg-slate-500">
+                            Product
+                        </li>
+                        <li 
+                            onClick={() => setOpenNav(false)}
+                        className=" hover:text-blue-700 transition-all cursor-pointer mt-4 text-white sm:text-black border sm:border-none border-gray-300 sm:p-0 p-2 rounded-md sm:bg-transparent bg-slate-500">
+                            <FiShoppingCart className="text-3xl" />
+                        </li>
+                    </ul>
+                    <div className="sm:hidden block">
+                        <FaBars 
+                            onClick={() => setOpenNav(true)}
+                        className="text-3xl" />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default NavBar;
