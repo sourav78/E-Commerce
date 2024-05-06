822f1f731adb4c26c78bcf56c7c93db667294397
sourav78 <souravranjan488@gmail.com>
2024-05-06 12:51:44 +0530

    frontend - making cart page price details design

--- EcommerceClient/src/components/Cart/PriceDetail.jsx
+++ EcommerceClient/src/components/Cart/PriceDetail.jsx
@@ -3,8 +3,44 @@ import React from 'react'
 const PriceDetail = () => {
     return (
         <>
-            <div className="bg-white py-8 px-8 shadow-xl sticky top-4">
-                
+            <div className="bg-white shadow-xl sticky top-4">
+                <div className=" p-4 border-b">
+                    <p className='font-semibold text-gray-600'>PRICE DETAILS</p>
+                </div>
+                <div className=" p-4">
+                    <div className="border-b pb-4 border-dashed">
+                        <div className="flex justify-between items-center">
+                            <p className='font-semibold text-gray-700'>Price</p>
+                            <p className='font-semibold text-lg text-gray-700'>₹34,668</p>
+                        </div>
+                        <div className="mt-3 flex justify-between items-center">
+                            <p className='font-semibold text-gray-700'>Discount</p>
+                            <p className='font-semibold text-lg text-green-600'>₹1234</p>
+                        </div>
+                        <div className="mt-3 flex justify-between items-center">
+                            <p className='font-semibold text-gray-700'>Delivery Charges</p>
+                            <p className='font-semibold text-lg text-gray-700'>₹332</p>
+                        </div>
+                    </div>
+                    <div className="border-b py-4 border-dashed">
+                        <div className="">
+                            <p className='font-semibold text-gray-700 mb-2'>COUPONS</p>
+                            <div className="flex justify-between items-center gap-2">
+                                <input className='outline-none border border-black focus:border-[#00ed64] px-2 py-1 flex-1' type="text" />
+                                <button className='px-4 py-1 border border-black bg-[#00ed64]'>Apply</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="border-b py-4 border-dashed">
+                        <div className="my-2 flex justify-between items-center">
+                            <p className='text-lg font-semibold'>Total Amount</p>
+                            <p className='text-lg font-semibold'>₹12,231</p>
+                        </div>
+                    </div>
+                    <div className="mt-2">
+                        <p className='text-green-600 font-semibold'>You will save ₹22,437 on this order</p>
+                    </div>
+                </div>
             </div>
         </>
     )
--- EcommerceClient/src/components/Cart/ProductCart.jsx
+++ EcommerceClient/src/components/Cart/ProductCart.jsx
@@ -17,7 +17,7 @@ const ProductCart = () => {
                 <div style={{ boxShadow: '0px -5px 10px rgba(0, 0, 0, 0.25)' }} className=" py-4 sm:px-8 px-4 shadow-md sticky bottom-0 bg-white">
                     <div className=" flex justify-between items-center">
                         <p className='text-3xl font-semibold'>₹4000</p>
-                        <button className='sm:px-16 px-12 py-4 border border-black text-xl font-semibold bg-[#00ed64]'>Place Order</button>
+                        <button className='sm:px-16 px-12 py-3 border border-black text-xl font-semibold bg-[#00ed64]'>Place Order</button>
                     </div>
                 </div>
             </div>
