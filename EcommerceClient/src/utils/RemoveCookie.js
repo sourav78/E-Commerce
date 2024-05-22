5ce306c583833b4e9e4ba368a2d81dbf50652fde
sourav78 <souravranjan488@gmail.com>
2024-05-22 17:45:00 +0530

    frontend - changes in cookie setup 2

--- EcommerceClient/src/utils/RemoveCookie.js
+++ EcommerceClient/src/utils/RemoveCookie.js
@@ -2,10 +2,7 @@ import Cookies from 'js-cookie'
 
 const RemoveCookie = (cookieName) => {
     return Cookies.remove(cookieName, {
-        path: '/', // Specify the path if it was set when the cookie was created
-        domain: 'https://s78-store.vercel.app', // Uncomment and set the domain if it was set when the cookie was created
-        secure: true, // Uncomment if the cookie was set with the secure flag
-        sameSite: 'Strict' // Uncomment if the cookie was set with the sameSite attribute
+        path: '/'
     });
 }
 
